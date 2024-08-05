54d3c789b3cef3d0a624282c0def94bd8989ef12
vtcong2299 <vtcong2299@gmail.com>
2024-08-05 21:15:21 +0700

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace THUCHANHTOANTU
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            int width;
+            int area;
+
+            Console.WriteLine("Nhap chieu cao: ");
+            height= Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap can nang: ");
+            width= Int32.Parse(Console.ReadLine());
+            area= height*width;
+            Console.WriteLine("Dien tich hinh chu nhat la: {0}", area);
+            
+        }
+    }
+}
